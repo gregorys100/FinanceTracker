50ab354a8d1b8859ba3772c9ed07a09b1f41d638
greg <gregorysequeira100@gmail.com>
2018-09-03 20:43:54 +0530

    2nd commit.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,11 @@
 package Main;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to my Finance Tracker!!");
+		System.out.println("\nWhat would you like to do?");
+		System.out.println("\n1. Make an entry \n2. View records");
+	}
 
 }
