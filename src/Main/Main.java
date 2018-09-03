ca87096e91b29ed0d109de5c0c6f4f32344ca48a
greg <gregorysequeira100@gmail.com>
2018-09-03 20:48:25 +0530

    2nd Continued

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,11 +1,18 @@
 package Main;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to my Finance Tracker!!");
 		System.out.println("\nWhat would you like to do?");
 		System.out.println("\n1. Make an entry \n2. View records");
+		Scanner sc = new Scanner(System.in);
+		sc.hasNext();
+		System.out.println(sc.nextLine());
 	}
 
 }
